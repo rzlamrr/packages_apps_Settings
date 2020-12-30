9b0460f26178a45df39e320d93c81f4e8b768991
elluzion <dyrex2004@gmail.com>
2020-12-31 00:10:03 +0100

    Settings: Redesign Hycon Version
    Add this to your device tree:
    
    PRODUCT_PRODUCT_PROPERTIES += \
      ro.hycon.maintainer=maintainername \
      ro.hycon.cpu=for example SDM855
    
    Co-authored-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/HyconVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/HyconVersionPreferenceController.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.deviceinfo.firmwareversion;
 
+import java.io.IOException;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
@@ -28,13 +29,20 @@ import android.util.Log;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
+import com.android.settings.utils.HyconSpecUtils;
 import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settings.slices.Sliceable;
 import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedLockUtilsInternal;
+import com.android.settingslib.widget.LayoutPreference;
+import com.android.settings.core.PreferenceControllerMixin;
+
+import android.widget.TextView;
 
 public class HyconVersionPreferenceController extends BasePreferenceController {
 
@@ -46,6 +54,27 @@ public class HyconVersionPreferenceController extends BasePreferenceController {
     private static final String ROM_RELEASETYPE_PROP = "org.pixelexperience.build_type";
     private static final String ROM_CODENAME_PROP = "org.hycon.codename";
 
+    @VisibleForTesting
+    TextView mHyconVersionText;
+    @VisibleForTesting
+    TextView mHyconVersionFlavourText;
+    @VisibleForTesting
+    TextView mDeviceNameText;
+    @VisibleForTesting
+    TextView mCpuText;
+    @VisibleForTesting
+    TextView mScreenResText;
+    @VisibleForTesting
+    TextView mBatteryText;
+    @VisibleForTesting
+    TextView mRamText;
+    @VisibleForTesting
+    TextView mMaintainerText;
+
+    private PreferenceFragmentCompat mHost;
+    private LayoutPreference mHyconVersionLayoutPref;
+    private Context mContext;
+
     private final UserManager mUserManager;
     private final long[] mHits = new long[ACTIVITY_TRIGGER_COUNT];
 
@@ -56,11 +85,16 @@ public class HyconVersionPreferenceController extends BasePreferenceController {
         super(context, key);
         mUserManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
         initializeAdminPermissions();
+        mContext = context;
     }
 
     @Override
     public int getAvailabilityStatus() {
         return AVAILABLE;
+    }
+
+    public void setFragment(PreferenceFragmentCompat fragment) {
+        mHost = fragment;
     }
 
     @Override
@@ -74,18 +108,70 @@ public class HyconVersionPreferenceController extends BasePreferenceController {
     }
 
     @Override
-    public CharSequence getSummary() {
-        String hyconVersion = SystemProperties.get(PROPERTY_HYCON_VERSION,
-                mContext.getString(R.string.unknown));
-        String hyconReleasetype =  SystemProperties.get(ROM_RELEASETYPE_PROP,
-                this.mContext.getString(R.string.unknown));
-        String hyconCodename = SystemProperties.get(ROM_CODENAME_PROP,
-                mContext.getString(R.string.unknown));
-        if (!hyconVersion.isEmpty() && !hyconReleasetype.isEmpty())
-            return hyconVersion + " | " + hyconCodename + " | " +  hyconReleasetype;
-        else
-            return mContext.getString(R.string.hycon_version_default);
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mHyconVersionLayoutPref = screen.findPreference(getPreferenceKey());
+        mHyconVersionText = mHyconVersionLayoutPref.findViewById(R.id.hycon_version);
+        mHyconVersionFlavourText = mHyconVersionLayoutPref.findViewById(R.id.hycon_version_flavour);
+        mDeviceNameText = mHyconVersionLayoutPref.findViewById(R.id.device_name_text);
+        mCpuText = mHyconVersionLayoutPref.findViewById(R.id.device_cpu_text);
+        mScreenResText = mHyconVersionLayoutPref.findViewById(R.id.device_screen_res_text);
+        mBatteryText = mHyconVersionLayoutPref.findViewById(R.id.device_battery_text);
+        mRamText = mHyconVersionLayoutPref.findViewById(R.id.device_ram_text);
+        mMaintainerText = mHyconVersionLayoutPref.findViewById(R.id.device_maintainer_text);
+
+        UpdateHyconVersionPreference();
     }
+
+    private void UpdateHyconVersionPreference() {
+        // We split the different specs into different voids to make the code more organized.
+        updateHyconVersionText();
+        updateDeviceNameText();
+        updateCpuText();
+        updateScreenResText();
+        updateBatteryText();
+        updateRamText();
+        updateMaintainerText();
+    }
+
+    private void updateHyconVersionText() {
+        String hyconVer = SystemProperties.get(PROPERTY_HYCON_VERSION);
+        String hyconType = SystemProperties.get(ROM_RELEASETYPE_PROP);
+        String hyconTypeCapitalized = hyconType.substring(0, 1).toUpperCase() + hyconType.substring(1).toLowerCase();
+        String[] hyconVerSeparated = hyconVer.split("-");
+
+        if (!hyconVer.isEmpty() && !hyconType.isEmpty()) {
+            mHyconVersionText.setText(hyconVerSeparated[0] + " " + hyconTypeCapitalized + " | ");
+            mHyconVersionFlavourText.setText(hyconVerSeparated[1]);
+        } else {
+            mHyconVersionText.setText("");
+            mHyconVersionFlavourText.setText(R.string.unknown);
+        }
+    }
+
+    private void updateDeviceNameText() {
+        mDeviceNameText.setText(HyconSpecUtils.getDeviceName());
+    }
+
+    private void updateBatteryText() {
+        mBatteryText.setText(HyconSpecUtils.getBatteryCapacity(mContext) + " mAh");
+    }
+
+    private void updateCpuText() {
+        mCpuText.setText(HyconSpecUtils.getProcessorModel());
+    }
+
+    private void updateScreenResText() {
+        mScreenResText.setText(HyconSpecUtils.getScreenRes(mContext));
+    }
+
+    private void updateRamText() {
+        mRamText.setText(String.valueOf(HyconSpecUtils.getTotalRAM())+ " GB");
+    }
+    private void updateMaintainerText() {
+        mMaintainerText.setText(String.valueOf(HyconSpecUtils.getMaintainerName()));
+    }
+
 
     @Override
     public boolean handlePreferenceTreeClick(Preference preference) {
