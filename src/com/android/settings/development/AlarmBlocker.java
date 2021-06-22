81e69c46eb727abc69ee89a203d51084d8341503
Introdructor <yashkachave7@gmail.com>
2021-06-22 15:48:05 +0000

    Settings:fixup

--- src/com/android/settings/development/AlarmBlocker.java
+++ src/com/android/settings/development/AlarmBlocker.java
@@ -165,7 +165,7 @@ public class AlarmBlocker extends SettingsPreferenceFragment {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.HAVOC_SETTINGS;
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
     }
 
     private boolean isFirstEnable() {
